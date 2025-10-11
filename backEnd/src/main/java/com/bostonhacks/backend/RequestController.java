ad1551f3f5ef8d1fd598be80e6ea568ab1222baa
alanaisi-ali <alanaisi@bu.edu>
2025-10-11 15:58:44 -0400

    importing for file usage

--- backEnd/src/main/java/com/bostonhacks/backend/RequestController.java
+++ backEnd/src/main/java/com/bostonhacks/backend/RequestController.java
@@ -4,6 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+
 @RestController
 public class RequestController {
   @GetMapping("/text")
