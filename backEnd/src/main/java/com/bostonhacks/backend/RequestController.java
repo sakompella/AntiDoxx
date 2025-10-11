456eacea30d660a279bdacc47da7c86f6d1f07dc
sakompella <87831937+sakompella@users.noreply.github.com>
2025-10-11 16:05:32 -0400

    api

--- backEnd/src/main/java/com/bostonhacks/backend/RequestController.java
+++ backEnd/src/main/java/com/bostonhacks/backend/RequestController.java
@@ -6,8 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RequestController {
+  public final static String API_KEY = "AIzaSyD6ev0oDBpe7_424-Jqkl4hZiJVx6cj4-o";
+
   @GetMapping("/text")
   public String getText() {
+    // todo
+    return "";
+  }
+
+  @GetMapping("/imageAdvice")
+  public String getImage(@RequestParam("")) {
 
   }
 }
